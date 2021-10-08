930c3bedba11e32aee39eab6fb320a2a863eb928
NinjaGrandmaster <arenas1944@gmail.com>
2021-10-08 13:44:10 -0700

    Simplified keeping track of document count

--- src/application/PostingUnit.java
+++ src/application/PostingUnit.java
@@ -4,16 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PostingUnit {
-    private int documentCount; // number of documents a term appears in
     private Map<String, Integer> postingMap; // contains the document id and number of times a term appears in that document
 
     public PostingUnit() {
-        this.documentCount = 0;
         this.postingMap = new HashMap<>();
     }
 
+    // return number of documents a term has appeared in
     public int getDocumentCount() {
-        return this.documentCount;
+        return this.postingMap.size();
     }
 
     public Map<String, Integer> getPostingMap() {
@@ -24,11 +23,6 @@ public class PostingUnit {
         // if the document exists in the posting map get the count for the term associated with it
         // else get default value zero
         int count = this.postingMap.getOrDefault(docID, 0);
-
-        // if count is zero term was found in a new document, thus increase the document count
-        if (count == 0) {
-            this.documentCount += 1;
-        }
 
         // add or update document term count in the posting map
         this.postingMap.put(docID, count + 1);
